6520f02353dfd50a908dbcd36d82c442f3bb33e0
yuqing <690230951@qq.com>
2020-10-25 19:51:43 +0800

    创建dev分支

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -38,6 +38,7 @@
           placeholder="请输入身份证号"
           :rules="[{ required: true, message: '请输入用户名' }]"
         />
+        
         <van-field
           v-model="username"
           name="用户名"
